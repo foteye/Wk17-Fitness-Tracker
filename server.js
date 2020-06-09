4c2a439bef9f7c63b2d9ae087a6e8c7e139c1401
Foti.Mougos <foti.mougos@gmail.com>
2020-06-09 21:20:20 +1000

    fcking mongo

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI);
 
 require("./routes/htmlRoutes.js")(app);
 require("./routes/apiRoutes.js")(app);
