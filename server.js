3eefd25c45f45ededcee4a24264b6229c8d64e07
Foti.Mougos <foti.mougos@gmail.com>
2020-06-09 21:29:47 +1000

    i am an idiot

--- server.js
+++ server.js
@@ -5,16 +5,24 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.static("public"));
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
-mongoose.connect(MONGODB_URI);
+const MONGOLAB_URI = process.env.MONGOLAB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGOLAB_URI, function (err, res) {
+    if (err) {
+        console.log('ERROR connecting to: ' + MONGOLAB_URI + '. ' + err);
+    } else {
+        console.log('Succeeded connected to: ' + MONGOLAB_URI);
+    }
+});
 
 require("./routes/htmlRoutes.js")(app);
 require("./routes/apiRoutes.js")(app);
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`This app is running on port ${PORT}!`);
+    console.log(`This app is running on port ${PORT}!`);
 });
